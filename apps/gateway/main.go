cdb2928f1b3920c7e33a750367d501c13ef83ebc
lidor <lidor1666689@gmail.com>
2024-03-22 18:31:25 +0200

    Start an nx monorepo project with go plugin

---
+++ apps/gateway/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load("config/.env")
+	port := os.Getenv("port")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world")
+	})
+
+	if err := http.ListenAndServe("localhost:"+port, mux); err != nil {
+		fmt.Println(err.Error())
+	}
+
+}
---
+++ apps/gateway/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+// TODO(lidor): need to add checks for all REST API endpoints
+func TestHello(t *testing.T) {
+	// result := Hello("world")
+	// if result != "Hello world" {
+	// 	t.Error("Expected Hello to append 'world'")
+	// }
+}
